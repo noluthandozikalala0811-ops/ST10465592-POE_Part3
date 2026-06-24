e1036c0a070651b2438f8b5cc003384a90ce2983
noluthandozikalala0811-ops <noluthandozikalala0811@gmail.com>
2026-06-24 15:25:04 +0200

    Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("active");
+}
+
+function openModal(){
+    document.getElementById("modal").style.display="flex";
+}
+
+function closeModal(){
+    document.getElementById("modal").style.display="none";
+}
